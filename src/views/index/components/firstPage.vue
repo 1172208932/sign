f5833727f63319cd50f290036d8640ee43afcd24
zhang xiaoxu <1172208932@qq.com>
2023-09-21 16:01:34 +0800

    feat: 添加lottieweb

--- src/views/index/components/firstPage.vue
+++ src/views/index/components/firstPage.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="first-page">
+        <div id="canvas1"></div>
     </div>
 </template>
     
@@ -7,6 +8,9 @@
 import { showToast } from "vant";
 import { ref, watch, onMounted, nextTick } from "vue";
 import { useRouter } from "vue-router";
+import lottie from 'lottie-web';
+import indexjson from './data.json'
+
 // import { postSignUp } from '@/api/resource'
 
 const router = useRouter();
@@ -24,9 +28,28 @@ let showPopup = ref<boolean>(false);
 //     showPopup.value = newProps.show;
 // });
 
-let animation
+let animation;
 
 onMounted(() => {
+      animation = lottie.loadAnimation({
+          container: document.getElementById('canvas1'),
+          renderer: 'svg',
+          loop: false,
+          autoplay: false,
+          animationData: indexjson,
+          assetsPath: 'src/assets/images/',
+      });
+      animation.addEventListener('complete', () => {
+            console.log('end')
+            // 设置开始帧和结束帧
+            animation.goToAndPlay(90, true)
+            // animation.stop()
+            // goToAndPlay(20, 40, true); // 替换为你想要的开始帧和结束帧
+
+            // // 重新播放动画
+            // animation.play();
+});
+
 })
 
 async function sureBtnClick() {
